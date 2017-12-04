c9127fb40fa93b7f2215238bf7f82046eceb341a
M.Tyrans <maximedu03120@hotmail.fr>
2017-12-04 12:44:50 +0100

    formulaire

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,87 +9,18 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+       <form>
+        <input type="file" name="nom" /><br/>
+        <input type="mail" name="mailfrom"><br/>
+        <input type="mail" name="mailsubmit"><br/>
+        <textarea name="content"></textarea><br/>
+        <button name="envoyer">Envoyer</button><br/>
+       </form>
+       <article>
+          <span>À propos de NomTransfert</span> 
+            <p>NomTransfert est le moyen le plus simple de transférer vos fichiers partout dans le monde. Chaque mois, notre service permet à des utilisateurs de 195 pays d'envoyer un milliard de fichiers. Fondé en 2009, NomTransfert a des équipes aux Pays-Bas et aux États-Unis.</p>
+       </article>
     </body>
 </html>
