5360f299d857a739f099504f3b496e4b8adf8934
M.Tyrans <maximedu03120@hotmail.fr>
2017-12-04 13:17:14 +0100

    lol

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,16 +11,12 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     </head>
     <body>
-       <form>
-        <input type="file" name="nom" /><br/>
-        <input type="mail" name="mailfrom"><br/>
-        <input type="mail" name="mailsubmit"><br/>
-        <textarea name="content"></textarea><br/>
-        <button name="envoyer">Envoyer</button><br/>
-       </form>
-       <article>
-          <span>À propos de NomTransfert</span> 
-            <p>NomTransfert est le moyen le plus simple de transférer vos fichiers partout dans le monde. Chaque mois, notre service permet à des utilisateurs de 195 pays d'envoyer un milliard de fichiers. Fondé en 2009, NomTransfert a des équipes aux Pays-Bas et aux États-Unis.</p>
-       </article>
+       <?php
+         echo Form::open(array('url' => '/uploadfile','files'=>'true'));
+         echo 'Select the file to upload.';
+         echo Form::file('image');
+         echo Form::submit('Upload File');
+         echo Form::close();
+      ?>
     </body>
 </html>
