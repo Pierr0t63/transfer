29c33bf0250a738b10566a5ade03350939f9737c
M.Tyrans <maximedu03120@hotmail.fr>
2017-12-05 14:06:15 +0100

    e

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -8,7 +8,7 @@
     <p>Un de vos contact vous a envoyé un fichier via nos services</p>
     <ul>
       <li><strong>Email du contact</strong> : {{ $contact['mail_from'] }}</li>
-      <li><strong>Lien du téléchargement</strong> : http://www.nontransfer.fr/telechargement?dossier={{ $contact['unique_name'] }}&fichier={{$contact['getClientOriginalName()']}}</li>
+      <li><strong>Lien du téléchargement</strong> : http://www.nontransfer.fr/telechargement?dossier=test&fichier=test</li>
     </ul>
   </body>
 </html>
