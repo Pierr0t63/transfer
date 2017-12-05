53cdbca375d54dad49642e03bd4a60fbf1284d3b
M.Tyrans <maximedu03120@hotmail.fr>
2017-12-05 13:57:51 +0100

    tt

--- app/Mail/Contact.php
+++ app/Mail/Contact.php
@@ -1,33 +1,26 @@
 <?php
-
+ 
 namespace App\Mail;
-
+ 
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
-
+ 
 class Contact extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public $contact;
+
+    public function __construct(Array $contact)
     {
-        //
+        $this->contact = $contact;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
+     public function build()
     {
-        return $this->view('view.name');
+        return $this->from('aformacvichy@gmail.com')
+            ->view('emails.contact');
     }
 }
---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h2>Nouveau ficher</h2>
+    <p>Un de vos contact vous a envoyé un fichier via nos services</p>
+    <ul>
+      <li><strong>Email du contact</strong> : {{ $contact['mail_from'] }}</li>
+      <li><strong>Lien du téléchargement</strong> : http://www.nontransfer.fr/telechargement?dossier={{ $contact['unique_name'] }}&fichier={{$contact['getClientOriginalName()']}}</li>
+    </ul>
+  </body>
+</html>
