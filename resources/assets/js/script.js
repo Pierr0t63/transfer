c2f795512c930493d6fe57aeaedaa82dc11dcc94
Pierrot <pierrot63200@mail.com>
2017-12-05 13:18:19 +0100

    Ajout Vegas.js (images du background)
    Ajout Vegas.js (images du background)

---
+++ resources/assets/js/script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+	$("body").vegas({
+		timer : false,
+	    slides: [
+	        { src: "/img/slide1.jpg" },
+	        { src: "/img/slide2.jpg" },
+	        { src: "/img/slide3.jpg" }
+	    ],
+	    overlay: "/img/overlays/08.png"
+	});
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,10 +13,13 @@ let mix = require('laravel-mix');
 
 mix.styles([
     'resources/assets/css/bootstrap.css',
-    'resources/assets/css/app.css'
+    'resources/assets/css/app.css',
+    'node_modules/vegas/dist/vegas.min.css'
 ], 'public/css/app.css')
 .scripts([
     'resources/assets/js/jquery-3.2.1.js',
     'resources/assets/js/popper.js',
-    'resources/assets/js/bootstrap.js'
+    'resources/assets/js/bootstrap.js',
+    'node_modules/vegas/dist/vegas.min.js',
+    'resources/assets/js/script.js'
 ], 'public/js/app.js');
